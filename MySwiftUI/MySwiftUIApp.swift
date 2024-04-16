6b8554ea5a34d0aff9678b13c8e316e8835d32da
Hendy <hendy_o@qq.com>
2024-04-16 16:10:01 +0800

    Initial Commit

---
+++ MySwiftUI/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  MySwiftUI
+//
+//  Created by Hendy Ou on 2024/4/16.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
